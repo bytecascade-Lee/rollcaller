5050c2a1d71438a7f28b875847c0119a94459f77
Serene Lee <serene2026@qq.com>
2026-07-22 16:42:33 +0800

    feat(app): 初始化应用配置和数据库并创建主窗口
    - 添加了应用路径和日志配置模块
    - 实现了数据库引导初始化功能
    - 创建了主窗口构建函数
    - 配置了 WebView 窗口大小为 800x600 并启用自动调整大小
    - 设置了应用程序数据目录路径
    - 将运行函数改为异步函数并添加错误处理

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -1,8 +1,34 @@
+use crate::config::{app_paths, logger};
+use crate::database::database_bootstrap;
+use tauri::WebviewWindowBuilder;
+
+mod cmd;
+mod common;
+mod config;
+mod database;
+mod repo;
+mod service;
+mod util;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub async fn run() {
+    logger::init();
+    database_bootstrap::init().await.expect("Failed to run database migrations.");
+
     tauri::Builder::default()
+        .setup(|app| create_window(app))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn create_window(app: &mut tauri::App) -> Result<(), Box<dyn std::error::Error>> {
+    WebviewWindowBuilder::new(app, "main", Default::default())
+        .data_directory(app_paths::webview_dir().to_path_buf())
+        .inner_size(800.0, 600.0)
+        .auto_resize()
+        .build()
+        .expect("error while creating window");
+    Ok(())
 }
