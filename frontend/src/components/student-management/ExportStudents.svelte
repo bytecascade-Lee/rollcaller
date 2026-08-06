befcb61c505a39f28c0856664a7495adced3680f
Serene Lee <serene2026@qq.com>
2026-08-06 11:06:55 +0800

    refactor(components): 移除导出学生组件中的冗余点击事件
    - 移除了弹窗遮罩层的无用点击关闭功能
    - 清理了重复的无障碍属性忽略注释
    - 简化了弹窗容器的交互逻辑
    - 提升了组件代码可读性
    - 优化了弹窗显示的样式隔离

--- frontend/src/components/student-management/ExportStudents.svelte
+++ frontend/src/components/student-management/ExportStudents.svelte
@@ -63,9 +63,6 @@
 {#if isVisible}
   <!-- svelte-ignore a11y_click_events_have_key_events -->
   <!-- svelte-ignore a11y_no_static_element_interactions -->
-  <div onclick={close}></div>
-  <!-- svelte-ignore a11y_click_events_have_key_events -->
-  <!-- svelte-ignore a11y_no_static_element_interactions -->
   <div
     class="popup"
     style={popoverStyle}
