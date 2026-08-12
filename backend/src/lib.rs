f96bd02a5115ce13622cb47bd4e4bbd505ada66d
Serene Lee <serene2026@qq.com>
2026-08-12 22:57:18 +0800

    feat(app): 在 lib.rs 中注册出勤状态的添加、修改和获取接口
    - 注册三个接口
    - 方便前端调用
    - 重命名未使用的变量 app、args 和 cmd

--- backend/src/lib.rs
+++ backend/src/lib.rs
@@ -17,8 +17,11 @@ pub async fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_tracing::Builder::new().build())
-        .plugin(tauri_plugin_single_instance::init(|app, args, cmd| {}))
+        .plugin(tauri_plugin_single_instance::init(|_app, _args, _cmd| {}))
         .invoke_handler(tauri::generate_handler![
+            crate::cmd::attendance_status::attendance_status_list,
+            crate::cmd::attendance_status::attendance_status_create,
+            crate::cmd::attendance_status::attendance_status_update,
             crate::cmd::app_info::app_info,
             crate::cmd::app_paths::data_dir,
             crate::cmd::app_paths::config_dir,
