973ea14cfbab1b88f78833e9acf85240a8cfc183
Serene Lee <serene2026@qq.com>
2026-08-15 21:40:40 +0800

    fix(cmd): 优化 README 和 LICENSE 渲染效果
    - help_load_readme 中移除 README 内的 en-US 版本跳转链接，避免出现跳转错误
    - help_load_license 中对 LICENSE 内容做格式化优化
      - 将 "MIT License" 替换为 Markdown 三级标题 "### MIT License"
      - 将版权声明包裹为斜体 "*Copyright (c) 2026 Serene Lee*"
    - 提升在 MarkdownView 组件中的阅读体验

--- backend/src/cmd/help.rs
+++ backend/src/cmd/help.rs
@@ -8,12 +8,17 @@ pub async fn help_load_markdown(id: String) -> Result<String, String> {
 
 #[tauri::command]
 pub async fn help_load_readme() -> Result<String, String> {
-    fs::read_to_string(&app_paths::resources_dir().parent().unwrap().join("README.md")).map_err(|e| e.to_string())
+    Ok(fs::read_to_string(&app_paths::resources_dir().parent().unwrap().join("README.md"))
+        .map_err(|e| e.to_string())?
+        .replace("> [en-US](README_en_US.md)", ""))
 }
 
 #[tauri::command]
 pub async fn help_load_license() -> Result<String, String> {
-    fs::read_to_string(&app_paths::resources_dir().parent().unwrap().join("LICENSE")).map_err(|e| e.to_string())
+    Ok(fs::read_to_string(&app_paths::resources_dir().parent().unwrap().join("LICENSE"))
+        .map_err(|e| e.to_string())?
+        .replace("MIT License", "### MIT License")
+        .replace("Copyright (c) 2026 Serene Lee", "*Copyright (c) 2026 Serene Lee*"))
 }
 
 #[tauri::command]
